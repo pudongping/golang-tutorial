20d553dccd9a453715e583ac58d407b18d5fcaa1
pudongping <276558492@qq.com>
2021-10-12 00:32:21 +0800

    切片的截取

---
+++ chapter01/07-slice/slice3.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 长度为 3，容量也为 3， [1, 2, 3]
+	s := []int{1, 2, 3}
+
+	// 截取 [0, 2) 长度的元素，亦即截取后的结果为 [1, 2]
+	s1 := s[0:2]
+	fmt.Println(s1) // [1 2]
+
+	// s1 虽然是通过截取的 s ，但是截取是深拷贝，修改截取后的切片，那么原始切片的值也会被改变
+	s1[0] = 100
+	fmt.Println(s)  // [100 2 3]
+	fmt.Println(s1) // [100 2]
+
+	// copy 函数可以将底层数组的 slice 一起进行拷贝
+	s2 := make([]int, 3) // s2 = [0 0 0]
+	fmt.Println(s2)
+
+	// 将 s 中的值，依次拷贝到 s2 中
+	copy(s2, s)
+	fmt.Println(s2)
+
+}
