3bec7b862b31b1302665f97f1d4b49ba9352ce42
pudongping <276558492@qq.com>
2021-10-17 02:18:51 +0800

    反射解析结构体标签 tag

---
+++ chapter01/10-reflect/README.md
@@ -0,0 +1,6 @@
+# 反射
+
+- [pair](./pair.go)
+- [通过反射获取变量的类型以及数据值](./reflect.go)
+- [通过反射获取结构体中的属性以及方法](./reflect1.go)
+- [反射解析结构体标签 tag](./reflect_tag.go)
---
+++ chapter01/10-reflect/reflect_tag.go
@@ -0,0 +1,35 @@
+/*
+反射解析结构体标签 tag
+ */
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type resume struct {
+	// 可以定义多个标签，通过空格做分割
+	Name string `info:"name" doc:"我的名字"`
+	Sex string `info:"sex"`
+}
+
+func findTag(str interface{})  {
+	t := reflect.TypeOf(str).Elem()
+
+	for i := 0; i < t.NumField(); i++ {
+		taginfo := t.Field(i).Tag.Get("info")
+		tagdoc := t.Field(i).Tag.Get("doc")
+		fmt.Println("info ==", taginfo, " doc ==", tagdoc)
+		// info == name  doc == 我的名字
+		// info == sex  doc ==
+	}
+
+}
+
+func main()  {
+
+	var re resume
+	findTag(&re)
+
+}
