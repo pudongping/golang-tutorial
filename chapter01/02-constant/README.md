16f624faddfa722b14b3b97f135fb9160b9da397
pudongping <276558492@qq.com>
2021-10-23 19:08:07 +0800

    常量的定义

--- chapter01/02-constant/README.md
+++ chapter01/02-constant/README.md
@@ -1,3 +1,4 @@
 # 常量
 
 - [声明常量的方式以及 iota](./constant.go)
+- [常量的定义](./constant1.go)
---
+++ chapter01/02-constant/constant1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+const Pi float64 = 3.14159265358979323846
+const zero = 0.0 // 无类型浮点常量
+const (          // 通过一个 const 关键字定义多个常量，和 var 类似
+	size int64 = 1024
+	eof        = -1 // 无类型整型常量
+)
+const u, v float32 = 0, 3   // u = 0.0, v = 3.0，常量的多重赋值
+const a, b, c = 3, 4, "foo" // a = 3, b = 4, c = "foo", 无类型整型和字符串常量
+
+func main() {
+
+	fmt.Println("Pi =", Pi)     // Pi = 3.141592653589793
+	fmt.Println("zero =", zero) // zero = 0
+	fmt.Println("size =", size) // size = 1024
+
+}
