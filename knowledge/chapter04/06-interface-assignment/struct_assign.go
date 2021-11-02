1648d52711a835dbdb68c61314246273eebe478d
pudongping <276558492@qq.com>
2021-11-03 00:31:05 +0800

    将类实例赋值给接口

---
+++ knowledge/chapter04/06-interface-assignment/struct_assign.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Integer int
+
+// 加法运算
+func (a *Integer) Add(b Integer) {
+	*a = (*a) + b
+}
+
+// 乘法运算
+func (a Integer) Multiply(b Integer) Integer {
+	return a * b
+}
+
+type Math interface {
+	Add(i Integer)
+	Multiply(i Integer) Integer
+}
+
+func main() {
+	var a Integer = 1
+	var m Math = &a
+	m.Add(2)
+	// 1 + 2 = 3
+	fmt.Printf("1 + 2 = %d\n", a)
+}
