7eb79cbe5a58b944e0759a4e86eb962c18b3c057
pudongping <276558492@qq.com>
2021-12-31 17:48:00 +0800

    Update channel.go

--- knowledge/chapter06/02-channel/channel.go
+++ knowledge/chapter06/02-channel/channel.go
@@ -8,9 +8,12 @@ import "fmt"
 func main() {
 
 	// 定义一个 channel
+	// 定义一个无缓冲的 channel
 	c := make(chan int)
 
 	go func() {
+		// 这里的 defer 一定在 `num := <-c` 这段代码后执行
+		// 因为如果主进程一直不读取 channel 变量 c 的数据的话，此时子 goroutine 一直是阻塞的
 		defer fmt.Println("goroutine 结束")
 
 		fmt.Println("goroutine 正在运行……")
